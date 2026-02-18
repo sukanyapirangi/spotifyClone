61b548c0eeca17fece0fbbb0bb9d8d83540c11c9
Sukanya_Pirangi <sukanyapirangi18@gmail.com>
2026-02-18 19:55:09 +0530

    updated route

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const cookieParser = require('cookieparser');
+const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/auth.routes')
-
+const musiRoutes = require('./routes/music.routes')
 const app = express();
 app.use(express.json());
+
 app.use(cookieParser());
 
 app.use('/api/auth',authRoutes);
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const authController = require('../controllers/auth.controller');
 
-const router = express.Router;
+const router = express.Router();
+
+router.post('/register',authController.registerUser)
+router.post('/login',authController.loginUser)
 
 module.exports = router;
